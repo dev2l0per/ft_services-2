907b7346ff97086f7e31414a0801269e2f6df825
Sanghyeok Nam <sanam@c2r10s7.42seoul.kr>
2020-07-06 19:14:27 +0900

    2020. 07. 06. Wordpress...

--- srcs/wordpress/srcs/init-wordpress.sh
+++ srcs/wordpress/srcs/init-wordpress.sh
@@ -2,3 +2,8 @@
 
 sleep 5
 mysql --host=mysql --user=admin --password=tkdgur123 wordpress < /tmp/wordpress.sql
+until [ $? != 0 ]
+do
+	sleep 1
+	mysql --host=mysql --user=admin --password=tkdgur123 wordpress < /tmp/wordpress.sql
+done
--- srcs/wordpress/srcs/run.sh
+++ srcs/wordpress/srcs/run.sh
@@ -2,7 +2,6 @@
 
 export WORD_IP=$(cat /tmp/ip)
 sed -i "s/WORD_IP/$WORD_IP/g" /etc/wordpress/wp-config.php
-sed -i "s/WORD_IP/$WORD_IP/g" /tmp/wordpress.sql
-
 sleep 5
-sh /tmp/init-wordpress.sh & php -S 0.0.0.0:5050 -t /etc/wordpress/
+php -S 0.0.0.0:5050 -t /etc/wordpress/
+#sh /tmp/init-wordpress.sh & php -S 0.0.0.0:5050 -t /etc/wordpress/
